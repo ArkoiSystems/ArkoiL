47ff32655b9cc043cd65584198b6d9175d5a4a86
Excse <28984027+Excse@users.noreply.github.com>
2020-04-19 23:08:00 +0200

    Some changes

---
+++ ArkoiSystems/arkoi-systems/build/precache-manifest.5333c6c85e79e1d4ca9696d48293253b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b50b6e0376bd93fd30a6d6c53396f694",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d36c38dd8fe00a1d9cdd",
+    "url": "/static/css/main.7499a387.chunk.css"
+  },
+  {
+    "revision": "aa7e75b8696bf9108d94",
+    "url": "/static/js/2.5df08c8a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.5df08c8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d36c38dd8fe00a1d9cdd",
+    "url": "/static/js/main.74af7b07.chunk.js"
+  },
+  {
+    "revision": "ff0f5d73072b9ffee934",
+    "url": "/static/js/runtime-main.63709cd7.js"
+  }
+]);
