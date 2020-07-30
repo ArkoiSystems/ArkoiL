6746a3d2390f8f9bedfc9c19c5164a4948d0c9af
Excse <28984027+Excse@users.noreply.github.com>
2020-07-31 00:12:03 +0200

    Forgot to remove the java project out of the compiler directory. (v2 ._.)

---
+++ compiler/c++/cmake-build-debug/CMakeFiles/ArkoiCompiler.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "ArkoiCompiler"
+  "ArkoiCompiler.pdb"
+  "CMakeFiles/ArkoiCompiler.dir/compiler/compiler.cpp.o"
+  "CMakeFiles/ArkoiCompiler.dir/lexer/lexer.cpp.o"
+  "CMakeFiles/ArkoiCompiler.dir/lexer/token.cpp.o"
+  "CMakeFiles/ArkoiCompiler.dir/main.cpp.o"
+  "CMakeFiles/ArkoiCompiler.dir/parser/parser.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ArkoiCompiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
