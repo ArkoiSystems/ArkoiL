9779cffe10f74d6db86b95501b7483fabf5738fa
Excse <28984027+Excse@users.noreply.github.com>
2020-09-07 18:54:38 +0200

    No special changes.

--- compiler/c++/src/parser/symboltable.h
+++ compiler/c++/src/parser/symboltable.h
@@ -16,6 +16,7 @@ class SymbolTable {
 
     typedef std::vector<std::shared_ptr<ASTNode>> Symbols;
     typedef std::unordered_map<std::string, Symbols> Table;
+    typedef std::function<bool(const std::shared_ptr<ASTNode> &)> Predicate;
 
 public:
     std::shared_ptr<SymbolTable> parent;
@@ -27,13 +28,11 @@ public:
     SymbolTable(const SymbolTable &other) = default;
 
 public:
-    std::shared_ptr<Symbols>
-    all(const std::string &id,
-        const std::function<bool(const std::shared_ptr<ASTNode> &)> &predicate);
+    std::shared_ptr<SymbolTable::Symbols> all(const std::string &id,
+                                              const SymbolTable::Predicate &predicate);
 
-    std::shared_ptr<Symbols>
-    scope(const std::string &id,
-          const std::function<bool(const std::shared_ptr<ASTNode> &)> &predicate);
+    std::shared_ptr<SymbolTable::Symbols> scope(const std::string &id,
+                                                const SymbolTable::Predicate &predicate);
 
     void insert(const std::string &id, const std::shared_ptr<ASTNode> &node);
 
